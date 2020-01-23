b235904ef09420f3015a1ec026d2f241be3d4084
sanketmaru <sanketmaru@gmail.com>
2020-01-23 15:41:35 +0530

    BrowserAnimationsModule added

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditorComponent } from './editor.component';
 import { EditableArticleResolver } from './editable-article-resolver.service';
 import { AuthGuard } from '../core';
@@ -8,7 +8,11 @@ import { SharedModule } from '../shared';
 import { EditorRoutingModule } from './editor-routing.module';
 
 @NgModule({
-  imports: [SharedModule, EditorRoutingModule],
+  imports: [
+    SharedModule,
+    EditorRoutingModule,
+    // BrowserAnimationsModule
+  ],
   declarations: [EditorComponent],
   providers: [EditableArticleResolver]
 })
